67f1dac6d59e6813f4b9d70155b447c4824edc89
Amir Beshkar <amir.beshkar@gmail.com>
2019-11-25 08:26:38 +0330

    add a new test to check the type of dataKey property

--- src/components/ListRows/index.test.tsx
+++ src/components/ListRows/index.test.tsx
@@ -7,6 +7,7 @@ interface TestRowProps {
   person: { code: number };
   name: string;
   family: string;
+  id: unknown;
 }
 
 function TestRow({ person: { code }, name, family }: TestRowProps) {
@@ -20,8 +21,8 @@ function TestRow({ person: { code }, name, family }: TestRowProps) {
 const sharedProps: ListRowsProps<TestRowProps> = {
   RowComponent: TestRow,
   rows: [
-    { person: { code: 11 }, name: "Sara", family: "Doe" },
-    { person: { code: 12 }, name: "John", family: "Doe" }
+    { person: { code: 11 }, name: "Sara", family: "Doe", id: 11 },
+    { person: { code: 12 }, name: "John", family: "Doe", id: 12 }
   ],
   dataKey: "person.code"
 };
@@ -35,8 +36,39 @@ describe("The ListRows component tests", () => {
     try {
       render(<ListRows {...sharedProps} dataKey="invalid.key" />);
     } catch (e) {
-      expect(e.message).toBe("The dataKey does not exist");
+      expect(e.message).toBe("The `invalid.key` property does not exist");
     }
+  });
+
+  test("It should throw an Error if the type of dataKey property is not number or string", () => {
+    const keys = [
+      { key: 11, error: false },
+      { key: "11", error: false },
+      { key: true, error: true },
+      { key: () => {}, error: true },
+      { key: new Date(), error: true },
+      { key: { test: { a: "b" } }, error: true }
+    ];
+
+    console.error = jest.fn();
+
+    keys.forEach(record => {
+      let hasError = false;
+      try {
+        render(
+          <ListRows
+            {...sharedProps}
+            RowProps={row => ({ ...row, id: record.key })}
+            dataKey="id"
+          />
+        );
+      } catch (e) {
+        hasError =
+          e.message === "The type of `id` property must be string or number";
+      }
+
+      expect(hasError).toBe(record.error);
+    });
   });
 
   test("It should render rows correctly", () => {
